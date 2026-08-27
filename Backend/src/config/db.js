3da98a709154c3442cc5b25dea5d9941492fad61
Wasiq Ashfaq <wasiqashfaq27@gmail.com>
2026-08-28 04:30:15 +0500

    fix: explicitly enforce dbName zeltxx in mongoose connection options

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
-const connectToMongo= async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("mongoose connected")
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            dbName: 'zeltxx'
+        })
+        console.log("mongoose connected to database: zeltxx")
     }
-    catch(error){
-        console.log("Mongo Not Connected" , error);
+    catch (error) {
+        console.log("Mongo Not Connected", error);
     }
 }
 
-export {connectToMongo};
+export { connectToMongo };
